a4f6b563f59a0920107f16c263d4156cbdadfdbd
Yuuki Ooguro <kitkat.jaja@gmail.com>
2018-09-08 20:28:59 +0900

    画像を成形して見た

--- src/main/java/com/ooogurooo/photoshare/infrastructure/CloudinaryClient.java
+++ src/main/java/com/ooogurooo/photoshare/infrastructure/CloudinaryClient.java
@@ -1,6 +1,7 @@
 package com.ooogurooo.photoshare.infrastructure;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
 import com.cloudinary.utils.ObjectUtils;
 
 import java.io.ByteArrayOutputStream;
@@ -12,7 +13,9 @@ public class CloudinaryClient {
     private Cloudinary cloudinary = new Cloudinary();
     
     public Map upload(InputStream inputStream) throws IOException {
-        return cloudinary.uploader().upload(bytesFrom(inputStream), ObjectUtils.emptyMap());
+        Transformation transformation = new Transformation().colorSpace("black").quality(80);
+        Map options = ObjectUtils.asMap("transformation", transformation);
+        return cloudinary.uploader().upload(bytesFrom(inputStream), options);
     }
 
     private byte[] bytesFrom(InputStream inputStream) throws IOException {
