e7fd065b9f8664231146c77bf14083b788a0d0e7
Yuuki Ooguro <kitkat.jaja@gmail.com>
2018-09-08 15:42:33 +0900

    画像に対応

--- src/main/java/com/ooogurooo/photoshare/controller/BotController.java
+++ src/main/java/com/ooogurooo/photoshare/controller/BotController.java
@@ -2,11 +2,14 @@ package com.ooogurooo.photoshare.controller;
 
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import java.io.IOException;
 
 @LineMessageHandler
 public class BotController {
@@ -15,7 +18,12 @@ public class BotController {
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);
         final String originalMessageText = event.getMessage().getText();
-        return new TextMessage(originalMessageText);
+        return new TextMessage("テキストです。");
+    }
+
+    @EventMapping
+    public Message handleImageMessageEvent(MessageEvent<ImageMessageContent> event) throws IOException {
+        return new TextMessage("画像です。");
     }
 
     @EventMapping
